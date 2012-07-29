053b9cc633a646dded07154d8d6bd83b6fb83baa
Krishna sagar <sagarkrishna14@gmail.com>
2012-07-29 18:18:31 +0530

    Read Me

---
+++ readme.pod.js
@@ -0,0 +1,3 @@
+<p>StickyHeader Table is an extremely light weight jquery plugin (< 2k - uncompressed) which makes lengthy tables more readable by fixing their header when the user scrolls the table.</p>
+
+<p>The aim of this plugin is to create a simple straight forward method to create sticky headers with your table. it will work on any elements scroll, not just on the window's scroll. You just need a line of code to do the magic.</p>
